6c939a715ea5e6aded5bc915180813c16c3bad14
Roland C. Dowdeswell <elric@imrryr.org>
2011-07-18 23:33:02 +0100

    Remove unnec. dependencies on Kharon.
    Change from die to confess.

--- tests/utils.pm
+++ tests/utils.pm
@@ -5,10 +5,7 @@ package tests::utils;
 use base qw(Exporter);
 @EXPORT_OK = qw{ compare_array compare_hash compare };
 
-use Kharon::Protocol::ArrayHash;
-use Kharon::Response;
-use Kharon::KeyValueResponse;
-
+use Carp;
 
 use strict;
 use warnings;
@@ -17,7 +14,7 @@ sub compare_array {
 	my ($lhs, $rhs) = @_;
 
 	if (@$lhs != @$rhs) {
-		die "ARRAY: arrays are of different length ". (@$lhs) .
+		confess "ARRAY: arrays are of different length ". (@$lhs) .
 		    " " . @$rhs;
 	}
 
@@ -31,14 +28,14 @@ sub compare_hash {
 
 	$rhs = \%{$rhs};
 	for my $i (keys %$lhs) {
-		die "$i exists in lhs but not rhs" if !exists($rhs->{$i});
+		confess "$i exists in lhs but not rhs" if !exists($rhs->{$i});
 
 		compare($lhs->{$i}, $rhs->{$i});
 		delete $rhs->{$i};
 	}
 
 	if (keys %$rhs) {
-		die join(', ', keys %$rhs) . " exist in rhs but not lhs";
+		confess join(', ', keys %$rhs) . " exist in rhs but not lhs";
 	}
 }
 
@@ -46,14 +43,14 @@ sub compare {
 	my ($lhs, $rhs) = @_;
 
 	if (ref($lhs) ne ref($rhs)) {
-		die ("REFS: '" . ref($lhs) . "' ne '" . ref($rhs) . "'!" );
+		confess ("REFS: '" . ref($lhs) . "' ne '" . ref($rhs) . "'!" );
 	}
 
 	return if !defined($lhs) && !defined($rhs);
 
 	if (ref($lhs) eq '') {
 		if ($lhs ne $rhs) {
-			die "SCALAR: $lhs ne $rhs!";
+			confess "SCALAR: $lhs ne $rhs!";
 		}
 		return;
 	}
@@ -61,7 +58,7 @@ sub compare {
 	return compare_array($lhs, $rhs) if ref($lhs) eq 'ARRAY';
 	return compare_hash($lhs, $rhs)  if ref($lhs) eq 'HASH';
 
-	die "REFS: can't interpret " . ref($lhs);
+	confess "REFS: can't interpret " . ref($lhs);
 }
 
 1;
