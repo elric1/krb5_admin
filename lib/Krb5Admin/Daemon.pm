50fd8190f3a3c28eea666134eb87d45c32e2718c
Roland C. Dowdeswell <elric@imrryr.org>
2012-08-01 23:06:39 +0100

    Add Krb5Admin::Daemon and Krb5Admin::ForkClient.

---
+++ lib/Krb5Admin/Daemon.pm
@@ -0,0 +1,107 @@
+#!/usr/pkg/bin/perl
+
+package Krb5Admin::Daemon;
+
+use IO::File;
+
+use Socket;
+use Sys::Syslog;
+
+use Kharon::Protocol::ArrayHash;
+use Kharon::Engine::Server v0.3;
+use Kharon::Entitlement::ACLFile;
+use Kharon::Entitlement::Equals;
+use Kharon::Entitlement::Object;
+use Kharon::Entitlement::Stack;
+use Kharon::Entitlement::SimpleSQL;
+
+use Krb5Admin::KerberosDB;
+use Krb5Admin::Log;
+
+use strict;
+use warnings;
+
+sub mk_kmdb {
+	my ($pes, $config, %args) = @_;
+	my @acls = ();
+	my $kmdb_class = 'Krb5Admin::KerberosDB';
+
+	my $acl     = Kharon::Entitlement::Stack->new();
+
+	my $objacl  = Kharon::Entitlement::Object->new();
+	push(@acls, $objacl);
+
+	my $sqlacl  = Kharon::Entitlement::SimpleSQL->new(
+	    table => 'krb5_admin_simple_acls');
+	$sqlacl->set_del_check(sub { $acl->check($_[0]); });
+	push(@acls, $sqlacl);
+
+	#
+	# When the ACL file is not present, don't bother with file ACL checks
+
+	if (defined($config->{acl_file}) && -f $config->{acl_file}) {
+		my $subacls = Kharon::Entitlement::Equals->new();
+		my $aclfile = Kharon::Entitlement::ACLFile->new(
+					filename => $config->{acl_file},
+					subobject => $subacls);
+		push(@acls, $aclfile);
+	}
+
+	$acl->set_subobjects(@acls);
+	$acl->set_creds($args{CREDS});
+	$pes->set_acl($acl);
+
+	my %kmdb_args = (
+		acl			=> $acl,
+		sacls			=> $sqlacl,
+		client			=> $args{CREDS},
+		addr			=> $args{REMOTE_IP},
+		allow_fetch		=> $config->{allow_fetch},
+		xrealm_bootstrap	=> $config->{xrealm_bootstrap},
+		win_xrealm_bootstrap	=> $config->{win_xrealm_bootstrap},
+		prestash_xrealm		=> $config->{prestash_xrealm},
+		sqlite			=> $config->{sqlite},
+		dbname			=> $config->{dbname},
+	);
+#	syslog('info', '%s connected from %s', $args{CREDS},
+#	    $args{REMOTE_IP});
+
+	$kmdb_class = $config->{kmdb_class} if defined($config->{kmdb_class});
+	my $ret = $kmdb_class->new(%kmdb_args);
+
+	$objacl->set_subobject($ret);
+	$sqlacl->set_dbh($ret->get_dbh());
+
+	return $ret;
+}
+
+sub run {
+	my ($config, %inargs) = @_;
+
+	my $creds	= $inargs{CREDS};
+	   $creds	= $ENV{KNC_CREDS}	if !defined($creds);
+	my $remote_ip	= $inargs{REMOTE_IP};
+	   $remote_ip	= $ENV{KNC_REMOTE_IP}	if !defined($remote_ip);
+
+	my $logger = $config->{logger};
+	$logger = Krb5Admin::Log->new()		if !defined($logger);
+
+	my $ahr = Kharon::Protocol::ArrayHash->new(banner => {version=>'2.0'});
+	my $pes = Kharon::Engine::Server->new(protocols => [$ahr],
+	    logger => $logger);
+	$pes->Connect();
+
+	my %args;
+	$args{master} = $config->{master}	if defined($config->{master});
+
+	if (defined($config->{preforked}) && $config->{preforked}) {
+		$args{object}	= sub { mk_kmdb($pes, $config, @_) };
+		$pes->RunKncAcceptor(%args);
+	} else {
+		$args{object}	= mk_kmdb($pes, $config, CREDS => $creds,
+		    REMOTE_IP => $remote_ip);
+		$pes->RunObj(%args);
+	}
+}
+
+1;
---
+++ lib/Krb5Admin/ForkClient.pm
@@ -0,0 +1,56 @@
+# 
+#
+# Blame: "Roland C. Dowdeswell" <elric@imrryr.org>
+
+package Krb5Admin::ForkClient;
+use base qw(Krb5Admin Kharon::Class::Client);
+
+use Kharon::Protocol::ArrayHash;
+use Kharon::Engine::Server;
+use Kharon::Engine::Client::Fork;
+use Kharon::Entitlement::Object;
+use Kharon::Entitlement::Stack;
+use Kharon::Entitlement::SimpleSQL;
+
+use Krb5Admin::Daemon;
+use Krb5Admin::KerberosDB;
+
+use POSIX;
+use Sys::Hostname;
+
+use strict;
+use warnings;
+
+sub run_daemon {
+	my ($fh, $config, %args) = @_;
+
+	dup2($fh->fileno(), 0);
+	dup2($fh->fileno(), 1);
+
+	$config->{master} = hostname();
+
+	Krb5Admin::Daemon::run($config, %args);
+	exit(0);
+}
+
+sub new {
+	my ($proto, $config, %args) = @_;
+	my $class = ref($proto) || $proto;
+	my $self;
+
+	$args{REMOTE_IP} = 'SOCKETPAIR'	if !defined($args{REMOTE_IP});
+
+	my $ahr = Kharon::Protocol::ArrayHash->new(banner => {version=>'2.0'});
+	my $pec = Kharon::Engine::Client::Fork->new(protocols => [$ahr]);
+
+	my ($kid, $fh) = $pec->Connect();
+
+	run_daemon($fh, $config, %args) if $kid == 0;
+
+	$self->{pec} = $pec;
+	$self->{kid} = $kid;
+
+	bless($self, $class);
+}
+
+1;
