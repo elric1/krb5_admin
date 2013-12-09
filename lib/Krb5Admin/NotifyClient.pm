e9bc056a75857338bdf718a4289a0a6a1af346f7
Matt Woodyard <Matthew.Woodyard@twosigma.com>
2013-12-09 19:53:47 +0000

    Change error exit code

--- lib/Krb5Admin/NotifyClient.pm
+++ lib/Krb5Admin/NotifyClient.pm
@@ -12,8 +12,9 @@ use Krb5Admin::Krb5Host::Client;
 my $SENDMAIL="/usr/sbin/sendmail";
 my $CF="/etc/postfix-prestash";
 
-# This should fork exec maybe?
-# host to specify the client cred
+
+#
+
 sub notify_update_required {
     my ($host) = @_;
     my $host_email = "notify\@$host"; 
