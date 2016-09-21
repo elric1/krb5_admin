elric1/krb5_admin

commit 1c411fe5df6907bd187b8b950a9e2b321f57a6bf

We implement Krb5Admin::Local and start using it.
The reason for this is so that we have a local object which behaves
more like the remote object: we attach input validation to it so that
it behaves more like the remote client.  We will look into attaching
logging and the like.

lib/Krb5Admin/Daemon.pm
@@ -18,6 +18,7 @@ use Kharon::InputValidation::Object;
 
 use Krb5Admin::KerberosDB;
 use Krb5Admin::Log;
+use Krb5Admin::Utils qw/mk_kmdb_with_config/;
 
 use strict;
 use warnings;
@@ -25,7 +26,6 @@ use warnings;
 sub mk_kmdb {
 	my ($pes, $config, %args) = @_;
 	my @acls = ();
-	my $kmdb_class = 'Krb5Admin::KerberosDB';
 
 	my $acl     = Kharon::Entitlement::Stack->new();
 
@@ -61,28 +61,14 @@ sub mk_kmdb {
 	$acl->set_creds($args{CREDS});
 	$pes->set_acl($acl);
 
-	my %kmdb_args = (
-		acl			=> $acl,
-		sacls			=> $sqlacl,
-		client			=> $args{CREDS},
-		addr			=> $args{REMOTE_IP},
-		allow_fetch		=> $config->{allow_fetch},
-		allow_fetch_old		=> $config->{allow_fetch_old},
-		enable_host_subdomain	=> $config->{enable_host_subdomain},
-		xrealm_bootstrap	=> $config->{xrealm_bootstrap},
-		win_xrealm_bootstrap	=> $config->{win_xrealm_bootstrap},
-		prestash_xrealm		=> $config->{prestash_xrealm},
-		sqlite			=> $config->{sqlite},
-		dbname			=> $config->{dbname},
-	);
-
 	if (defined($args{CREDS}) && defined($args{REMOTE_IP})) {
 		$config->{logger}->log('info', $args{CREDS} .
 		    ' connected from ' .  $args{REMOTE_IP});
 	}
 
-	$kmdb_class = $config->{kmdb_class} if defined($config->{kmdb_class});
-	my $ret = $kmdb_class->new(%kmdb_args);
+	$args{acl}   = $acl;
+	$args{sacls} = $sqlacl;
+	my $ret = mk_kmdb_with_config($config, \%args);
 
 	$objacl->set_subobject($ret);
 	$sqlacl->set_dbh($ret->get_dbh());

lib/Krb5Admin/Local.pm
@@ -1,0 +1,64 @@
+#
+#
+# Blame: "Roland C. Dowdeswell" <elric@imrryr.org>
+
+package Krb5Admin::Local;
+
+use base qw(Kharon::Class::Local);
+
+use Kharon::InputValidation::Object;
+
+use Krb5Admin::KerberosDB;
+use Krb5Admin::Utils qw/mk_kmdb_with_config/;
+
+use strict;
+use warnings;
+
+sub new {
+	my ($proto, $config, $args) = @_;
+	my $class = ref($proto) || $proto;
+
+	$args->{local} = 1;
+	my $kmdb = mk_kmdb_with_config($config, $args);
+
+	my $iv = Kharon::InputValidation::Object->new(subobject => $kmdb);
+	my $self = $class->SUPER::new(obj => $kmdb, iv => $iv);
+
+	return bless($self, $class);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Krb5Admin::Local - locally manipulate a Kerberos DB
+
+=head1 SYNOPSIS
+
+	use Krb5Admin::Local;
+
+	my $kmdb = Krb5Admin::Client->new();
+
+=head1 DESCRIPTION
+
+=head1 CONSTRUCTOR
+
+=over 4
+
+=item new(%ARGS)
+
+Creates a new "Krb5Admin::Local" object.  The %ARGS are passed
+directly to Krb5Admin::KerberosDB without modification.
+
+=back
+
+=head1 METHODS
+
+All of the user-visible methods are inherited from Krb5Admin and are
+documented there as well.
+
+=head1 SEE ALSO
+
+L<Krb5Admin>

lib/Krb5Admin/Utils.pm
@@ -6,7 +6,8 @@
 package Krb5Admin::Utils;
 use Exporter;
 @ISA = qw(Exporter);
-@EXPORT_OK = qw/reverse_the host_list force_symlink/;
+@EXPORT_OK = qw/reverse_the host_list force_symlink
+		load_config mk_kmdb_with_config/;
 
 #
 # Host list will, given an IP/Hostname return a list of all of the valid
@@ -38,6 +39,68 @@ sub force_symlink {
 		rename("$from.$$", $from) or
 		    die "$0: Can't create $from as a link to $to: $!\n";
 	}
+}
+
+sub mk_kmdb_with_config {
+	my ($config, $args) = @_;
+	my $cf;
+
+	$cf = load_config($config->{config}, $config->{config_provided});
+
+	$cf->{acl_file}		= $config->{acl_file};
+	$cf->{dbname}		= $config->{dbname};
+	$cf->{master}		= $config->{master};
+	$cf->{preforked}	= $config->{preforked};
+	$cf->{sqlite}		= $config->{sqlite};
+
+	my $kmdb_class = 'Krb5Admin::KerberosDB';
+
+	my %kmdb_args = (
+		acl			=> $args->{acl},
+		sacls			=> $args->{sacls},
+		client			=> $args->{CREDS},
+		addr			=> $args->{REMOTE_IP},
+		local			=> $args->{local},
+		allow_fetch		=> $config->{allow_fetch},
+		allow_fetch_old		=> $config->{allow_fetch_old},
+		enable_host_subdomain	=> $config->{enable_host_subdomain},
+		xrealm_bootstrap	=> $config->{xrealm_bootstrap},
+		win_xrealm_bootstrap	=> $config->{win_xrealm_bootstrap},
+		prestash_xrealm		=> $config->{prestash_xrealm},
+		sqlite			=> $config->{sqlite},
+		dbname			=> $config->{dbname},
+	);
+
+	$kmdb_class = $config->{kmdb_class} if defined($config->{kmdb_class});
+	return $kmdb_class->new(%kmdb_args);
+}
+
+sub load_config {
+	my ($config, $provided) = @_;
+
+	return {}				if ! -f $config && !$provided;
+        die "Couldn't find $config\n"           if ! -f $config;
+
+        my $ret = do $config;
+        die "Couldn't parse $config: $@\n"      if $@;
+	die "Couldn't open $config: $!\n"	if !defined($ret);
+
+	my %config;
+	$config{acl_file}		= $acl_file;
+	$config{config}			= $config;
+	$config{dbname}			= $dbname;
+	$config{sqlite}			= $sqlite;
+	$config{allow_fetch}		= $allow_fetch;
+	$config{allow_fetch_old}	= $allow_fetch_old;
+	$config{subdomain_prefix}	= $subdomain_prefix;
+	$config{master}			= $master;
+	$config{kmdb_class}		= $kmdb_class;
+	$config{xrealm_bootstrap}	= \%xrealm_bootstrap;
+	$config{win_xrealm_bootstrap}	= \%win_xrealm_bootstrap;
+	$config{prestash_xrealm}	= \%prestash_xrealm;
+	$config{preforked}		= $opts{P};
+
+	return \%config;
 }
 
 1;

scripts/krb5_admind
@@ -5,6 +5,7 @@ use Sys::Hostname;
 use Sys::Syslog;
 
 use Krb5Admin::Daemon;
+use Krb5Admin::Utils qw/load_config/;
 
 use strict;
 use warnings;
@@ -28,52 +29,26 @@ sub usage {
 
 openlog('krb5_admind', 'pid', 'auth');
 
-our $acl_file = ACL_FILE;
-our $config = KRB5_ADMIND_CONFIG;
-our $dbname;
-our $sqlite;
-our $allow_fetch;
-our $allow_fetch_old;
-# If defined it should end in a dot
-our $subdomain_prefix;
-our $master;
-our $kmdb_class;
-our %xrealm_bootstrap;
-our %win_xrealm_bootstrap;
-our %prestash_xrealm;
+my $config = {};
+my $master;
+
+$config->{acl_file}	= ACL_FILE;
+$config->{config}	= KRB5_ADMIND_CONFIG;
 
 my %opts;
 getopts('D:MPS:a:c:d:m:', \%opts) or usage();
 
-$master   = hostname()		if  defined($opts{M});
-$master   = $opts{m}		if !defined($master);
-$acl_file = $opts{a}		if  defined($opts{a});
-$dbname   = $opts{d}		if  defined($opts{d});
-$dbname   = $opts{D}		if  defined($opts{D});
-$sqlite   = $opts{S}		if  defined($opts{S});
-$config   = $opts{c}		if  defined($opts{c});
+$master   = hostname()			if  defined($opts{M});
+$master   = $opts{m}			if !defined($master);
 
-if (-f $config || defined($opts{c})) {
-	my $ret = do $config;
-	die "Couldn't parse $config: $@\n"	if $@;
-	die "Couldn't find $config\n"		if ! -f $config;
-}
+$config->{master}	= $master;
+$config->{acl_file}	= $opts{a}	if  defined($opts{a});
+$config->{dbname}	= $opts{d}	if  defined($opts{d});
+$config->{dbname}	= $opts{D}	if  defined($opts{D});
+$config->{sqlite}	= $opts{S}	if  defined($opts{S});
+$config->{config}	= $opts{c}	if  defined($opts{c});
+$config->{preforked}	= $opts{P};
 
-$kmdb_class = 'Krb5Admin::KerberosDB' if !defined($kmdb_class);
+$config->{config_provided}	= defined($opts{c});
 
-my %config;
-$config{acl_file}		= $acl_file;
-$config{config}			= $config;
-$config{dbname}			= $dbname;
-$config{sqlite}			= $sqlite;
-$config{allow_fetch}		= $allow_fetch;
-$config{allow_fetch_old}	= $allow_fetch_old;
-$config{subdomain_prefix}	= $subdomain_prefix;
-$config{master}			= $master;
-$config{kmdb_class}		= $kmdb_class;
-$config{xrealm_bootstrap}	= \%xrealm_bootstrap;
-$config{win_xrealm_bootstrap}	= \%win_xrealm_bootstrap;
-$config{prestash_xrealm}	= \%prestash_xrealm;
-$config{preforked}		= $opts{P};
-
-Krb5Admin::Daemon::run(\%config) or usage();
+Krb5Admin::Daemon::run($config) or usage();

t/features.t
@@ -2,19 +2,18 @@
 
 use Test::More tests => 4;
 
-use Krb5Admin::KerberosDB;
+use Krb5Admin::Local;
 
 use strict;
 use warnings;
 
 $ENV{'KRB5_CONFIG'} = './t/krb5.conf';
 
-my $kmdb = Krb5Admin::KerberosDB->new(
-    local	=> 1,
+my $kmdb = Krb5Admin::Local->new({
     client	=> 'root@TEST.REALM',
     dbname	=> 'db:t/test-hdb',
     sqlite	=> 't/sqlite.db',
-);
+});
 
 $kmdb->add_feature('BAR-Feature');
 $kmdb->add_feature('BAZ-Feature');

t/krb5_host.t
@@ -4,7 +4,7 @@ use Test::More tests => 32;
 
 use Sys::Hostname;
 
-use Krb5Admin::KerberosDB;
+use Krb5Admin::Local;
 use Krb5Admin::Krb5Host::Local;
 
 use Data::Dumper;
@@ -142,12 +142,11 @@ sub get_kt {
 
 my $ctx = Krb5Admin::C::krb5_init_context();
 
-my $kmdb = Krb5Admin::KerberosDB->new(
-    local	=> 1,
+my $kmdb = Krb5Admin::Local->new({
     client	=> 'root@TEST.REALM',
     dbname	=> 'db:t/test-hdb',
     sqlite	=> 't/sqlite.db',
-);
+});
 
 do $KRB5_KEYTAB_CONFIG if -f $KRB5_KEYTAB_CONFIG;
 diag $@ if $@;

t/owners.t
@@ -4,7 +4,6 @@ use Test::More tests => 48;
 
 use Sys::Hostname;
 
-use Krb5Admin::KerberosDB;
 use Krb5Admin::ForkClient;
 
 use Data::Dumper;

commit f4f986c4a971fa67cb5535836cee0ad907187919

Move config loading earlier in the process.
We introduced a regression in 1c411fe5df6 where we delayed processing
of the config file until after some parts of it were needed.  We've
moved the processing back to a more reasonable point.

lib/Krb5Admin/Daemon.pm
@@ -18,7 +18,7 @@ use Kharon::InputValidation::Object;
 
 use Krb5Admin::KerberosDB;
 use Krb5Admin::Log;
-use Krb5Admin::Utils qw/mk_kmdb_with_config/;
+use Krb5Admin::Utils qw/load_config mk_kmdb_with_config/;
 
 use strict;
 use warnings;
@@ -89,6 +89,8 @@ sub connect_kmdb {
 
 sub run {
 	my ($config, %inargs) = @_;
+
+	$config = load_config($config);
 
 	$config->{logger} //= Krb5Admin::Log->new();
 

lib/Krb5Admin/Local.pm
@@ -9,7 +9,7 @@ use base qw(Kharon::Class::Local);
 use Kharon::InputValidation::Object;
 
 use Krb5Admin::KerberosDB;
-use Krb5Admin::Utils qw/mk_kmdb_with_config/;
+use Krb5Admin::Utils qw/load_config mk_kmdb_with_config/;
 
 use strict;
 use warnings;
@@ -17,6 +17,8 @@ use warnings;
 sub new {
 	my ($proto, $config, $args) = @_;
 	my $class = ref($proto) || $proto;
+
+	$config = load_config($config);
 
 	$args->{local} = 1;
 	my $kmdb = mk_kmdb_with_config($config, $args);

lib/Krb5Admin/Utils.pm
@@ -43,17 +43,6 @@ sub force_symlink {
 
 sub mk_kmdb_with_config {
 	my ($config, $args) = @_;
-	my $cf;
-
-	$cf = load_config($config->{config}, $config->{config_provided});
-
-	$cf->{acl_file}		= $config->{acl_file};
-	$cf->{dbname}		= $config->{dbname};
-	$cf->{master}		= $config->{master};
-	$cf->{preforked}	= $config->{preforked};
-	$cf->{sqlite}		= $config->{sqlite};
-
-	my $kmdb_class = 'Krb5Admin::KerberosDB';
 
 	my %kmdb_args = (
 		acl			=> $args->{acl},
@@ -71,37 +60,38 @@ sub mk_kmdb_with_config {
 		dbname			=> $config->{dbname},
 	);
 
-	$kmdb_class = $config->{kmdb_class} if defined($config->{kmdb_class});
+	my $kmdb_class   = $config->{kmdb_class};
+	   $kmdb_class //= 'Krb5Admin::KerberosDB';
+
 	return $kmdb_class->new(%kmdb_args);
 }
 
 sub load_config {
-	my ($config, $provided) = @_;
+	my ($config) = @_;
+	my $file = $config->{config};
+	my $provided = $config->{config_provided};
 
-	return {}				if ! -f $config && !$provided;
-        die "Couldn't find $config\n"           if ! -f $config;
+	return $config				if ! -f $file && !$provided;
+        die "Couldn't find $file\n"		if ! -f $file;
 
         my $ret = do $config;
-        die "Couldn't parse $config: $@\n"      if $@;
-	die "Couldn't open $config: $!\n"	if !defined($ret);
+        die "Couldn't parse $file: $@\n"	if $@;
+	die "Couldn't open $file: $!\n"		if !defined($ret);
 
-	my %config;
-	$config{acl_file}		= $acl_file;
-	$config{config}			= $config;
-	$config{dbname}			= $dbname;
-	$config{sqlite}			= $sqlite;
-	$config{allow_fetch}		= $allow_fetch;
-	$config{allow_fetch_old}	= $allow_fetch_old;
-	$config{subdomain_prefix}	= $subdomain_prefix;
-	$config{master}			= $master;
-	$config{maxconns}		= $maxconns;
-	$config{kmdb_class}		= $kmdb_class;
-	$config{xrealm_bootstrap}	= \%xrealm_bootstrap;
-	$config{win_xrealm_bootstrap}	= \%win_xrealm_bootstrap;
-	$config{prestash_xrealm}	= \%prestash_xrealm;
-	$config{preforked}		= $opts{P};
+	$config->{acl_file}		= $acl_file;
+	$config->{dbname}		= $dbname;
+	$config->{sqlite}		= $sqlite;
+	$config->{allow_fetch}		= $allow_fetch;
+	$config->{allow_fetch_old}	= $allow_fetch_old;
+	$config->{subdomain_prefix}	= $subdomain_prefix;
+	$config->{master}		= $master;
+	$config->{maxconns}		= $maxconns;
+	$config->{kmdb_class}		= $kmdb_class;
+	$config->{xrealm_bootstrap}	= \%xrealm_bootstrap;
+	$config->{win_xrealm_bootstrap}	= \%win_xrealm_bootstrap;
+	$config->{prestash_xrealm}	= \%prestash_xrealm;
 
-	return \%config;
+	return $config;
 }
 
 1;