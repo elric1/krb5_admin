elric1/krb5_admin

commit a6cda1b80127b4d0178d8a33baa9e2325a43cb58

Replace "list_commands" with a call to help

lib/Krb5Admin.pm
@@ -179,52 +179,6 @@ sub regenkeys {
 	};
 }
 
-# Consider loading from file
-our @KRB5_USER_COMMANDS = (
-"list                 - list all principals in the database",
-"list_labels          - list available host labels",
-"listpols             - list kdc policies",
-"mquery               - show details for principals matching a pattern",
-"list_commands        - show this help",
-"query                - show details for a specific principal ",
-"query_acl            - show information about a krb5_admin user",
-"query_aclgroup       - show membership information about krb5_admin groups",
-"query_host           - show information about a host",
-"query_hostmap        - show cluster memberships",
-"sacls_query          - list the assigned administrative privileges",
-"add_acl              - create a krb5_admin user or group",
-"add_label            - add a label",
-"bind_host            - associate a boostrap id with a host principal name",
-"change_passwd        - change a password",
-"create_appid         - create a new appid, the kerberos principal of a " .
-			"role account",
-"create_host          - create a new entry for a host",
-"create_user          - create a new kerberos user",
-"del_acl              - delete a krb5_admin user or group",
-"del_label            - remove a label",
-"disable              - disable a principal",
-"enable               - enable a principal",
-"insert_aclgroup      - add a member to a group",
-"insert_hostmap       - add a host to a cluster",
-"modify               - modify an appid (owners, etc)",
-"modify_host          - modify properties of a host(ip, binding, labels, " .
-			"etc) ",
-"remove               - remove a principal from the database ",
-"remove_aclgroup      - remove a member from a krb5_admin group",
-"remove_host          - remove a host",
-"remove_hostmap       - remove a host from a cluster ",
-"reset_passwd         - reset a kerberos password",
-"sacls_add            - add an administrative privilege",
-"sacls_del            - remove an administrative privilege",
-"add_acl_owner        - add an owner to an acl or aclgroup",
-"remove_acl_owner     - remove an owner from an aclgroup",
-"query_acl_owner      - list the owner of an aclgroup",
-"create_logical_host  - create a logical host ",
-"new_host_secret      - create new master secret",
-"bind_host_secret     - create/change host secret",
-"read_host_secret     - return the host's current secret",
-);
-
 1;
 
 __END__

lib/Krb5Admin/CLI.pm
@@ -90,6 +90,7 @@ sub KHARON_COMMAND_ALIASES {
 	return {
 		is_owner	=> 'is_appid_owner',
 		is_pwner	=> 'is_appid_owner',
+		list_commands	=> 'help',
 		reset_password	=> 'reset_passwd',
 	};
 }
@@ -141,18 +142,21 @@ Commands that operate on hosts:
 	create_host		create a host
 	create_logical_host	create a logical host, i.e. a cluster
 	bind_host		assign a ``bootbinding'' to a host
-	remove_host		remove a host
 	modify_host		modify the attributes of a host
+	query_host		show information about a host
+	remove_host		remove a host
+
+Commands that operate on Simple ACLS (sacls):
+
+	sacls_add		add a sacl
+	sacls_del		remove a sacl
+	sacls_query		show or list sacls
 
 Commands that operate on host secrets:
 
 	new_host_secret		create new master secret
 	bind_host_secret	create/change host secret
 	read_host_secret	return the host's current secret
-
-Show a list of available commands:
-
-	list_commands
 
 For a more complete description of commands with usage and examples,
 please refer to the manual page which can be accessed via:

lib/Krb5Admin/KerberosDB.pm
@@ -3500,11 +3500,6 @@ sub query_acl_owner {
 	return $r;
 }
 
-sub KHARON_ACL_list_commands { return 1; }
-sub list_commands {
-	return @Krb5Admin::KRB5_USER_COMMANDS;
-}
-
 sub KHARON_IV_principal_map_remove {
 	my ($self, $cmd, $account, $svc, $hostname) = @_;
 	my $usage = "$cmd <account> <service> <hostname>";

commit 56e9c8472a560fa536578b323ca4e2f78688c4c5

fix bug introduced in a6cda1b80127.

lib/Krb5Admin.pm
@@ -89,7 +89,6 @@ our @KHARON_RO_AC_EXPORT = qw/	fetch
 				list_subject
 				listpols
 				mquery
-				list_commands
 				list_table
 				search_group
 				search_host